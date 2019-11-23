d7c0f528e865eefac4f56cff62a2d59003620ca0
Tiago Bagni <tibagni@gmail.com>
2019-11-23 13:49:30 -0300

    Create install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo ================================================== 
+echo Installing dotfiles...
+echo ================================================== 
+echo
+echo
+
+deldotVimrc=n
+delVimrc=n
+if [ -f ~/.vimrc ]
+then
+    read -p ".vimrc exists on your home. Override? (y/n) " -n1 deldotVimrc
+    echo
+fi
+
+if [ -f ~/.vim/vimrc ]
+then
+    read -p "vimrc exists on your .vim folder. Override? (y/n) " -n1 delVimrc
+    echo
+fi
+
+if [ $deldotVimrc = "y" ]
+then
+    echo "deleting current ~/.vimrc file..."
+    rm ~/.vimrc
+fi
+
+if [ $delVimrc = "y" ]
+then
+    echo "deliting current ~/.vim/vimrc file..."
+    rm ~/.vim/vimrc
+fi
+
+if [ ! -d ~/.vim ]
+then
+    echo "~/.vim does not exist. Creating..."
+    mkdir ~/.vim
+fi
+
+echo "installing ~/.vim/vimrc..."
+cp vimrc ~/.vim/vimrc
+
+echo done!
